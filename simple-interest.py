97b1a53dcd7eef157953055f2ef53e5e7659e3ac
ArifAnsari5296 <91407450+ArifAnsari5296@users.noreply.github.com>
2023-07-06 01:42:44 +0530

    Create simple-interest.py

---
+++ simple-interest.py
@@ -0,0 +1,7 @@
+principle=int(input("Enter your Amount :"))
+Time=int(input("enter time taken :"))
+rate=int(input("enter your rate of interest :"))
+
+simple_interest=principle*Time*rate/100
+
+print("your interest will be :",simple_interest)
